9ddc9472f23a3681fe432efbde06ef2177aa3eaa
Suo Jianfei <suojianfei@bestseller.com.cn>
2024-05-30 15:40:41 +0800

    初始化插件

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-module.exports = require('./lib/q-spore');
+'use strict';
+
+/**
+ * 本文件是一个入口文件，用于导出 lib 下所有可使用工具
+ */
+
+// 导出 Q-Spore
+const Q = require('./lib/q-spore');
+
+// 导出 rabbitMq
+const rabbitMq = require('./lib/rabbitMq');
+
+// 导出 sqlite
+const sqlite = require('./lib/sqlite');
+
+
+moudle.exports = {
+    Q,
+    rabbitMq,
+    sqlite
+}
