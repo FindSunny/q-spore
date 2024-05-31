48dab64be7d6c15abfae5518e6174899e9eadcfb
Suo Jianfei <suojianfei@bestseller.com.cn>
2024-05-31 11:39:18 +0800

    添加工具类

--- index.js
+++ index.js
@@ -6,13 +6,10 @@
 
 // 导出 Q-Spore
 const Q = require('./lib/q-spore');
-
 // 导出 rabbitMq
 const rabbitMq = require('./lib/rabbitMq');
-
 // 导出 sqlite
 const sqlite = require('./lib/sqlite');
-
 
 moudle.exports = {
     Q,
